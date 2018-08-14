41f13050ee5ff291a0484cdf88227279d6a1f3c1
Alan Piovezan <alan.log.piovezan@gmail.com>
2018-08-14 11:33:02 -0300

    commit inicial

---
+++ src/br/com/redmonkey/project/CalculadoraBean.java
@@ -0,0 +1,55 @@
+package br.com.redmonkey.project;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CalculadoraBean {
+
+	
+	private Double valorA;
+	private Double valorB;
+	private Double resultado;
+	
+	public double somar () {
+		resultado =this.valorA+this.valorB;
+		
+		return resultado;
+	}
+	public double subtrair () {
+		resultado =this.valorA-this.valorB;
+		
+		return resultado;
+	}
+	public double multiplicar () {
+		resultado =this.valorA*this.valorB;
+		
+		return resultado;
+	}
+	public double dividir () {
+		resultado =this.valorA/this.valorB;
+		
+		return resultado;
+	}
+	
+	public Double getValorA() {
+		return valorA;
+	}
+	public void setValorA(Double valorA) {
+		this.valorA = valorA;
+	}
+	public Double getValorB() {
+		return valorB;
+	}
+	public void setValorB(Double valorB) {
+		this.valorB = valorB;
+	}
+	public Double getResultado() {
+		return resultado;
+	}
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+	
+	
+	
+}
